7097db1c42bb334b09421a84eda33784f3c1140b
Dhruv <dlakhiani02@gmail.com>
2021-12-08 18:39:41 -0500

    teleop

--- scripts/teleop_twist_key.py
+++ scripts/teleop_twist_key.py
@@ -1,137 +1,93 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+
 from geometry_msgs.msg import Twist
 
-import tty
-import termios
-import select
 import sys
-import rospy
-import threading
-import roslib
-
-roslib.load_manifest('teleop_twist_keyboard')
-
+import select
+import termios
+import tty
 
 msg = """
+Control Your Turtlebot!
 ---------------------------
-Holonomic Movement:
-        w     
-   a    k    d
-        s     
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
 
-q/z: increase/decrease max speeds by 10%
-e/c: increase/decrease linear speed by 10%
----------------------------
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+space key, k : force stop
+anything else : stop smoothly
+
+CTRL-C to quit
 """
 
 moveBindings = {
-    'w': (1, 0, 0, 0),
-    'a': (0, 1, 0, 0),
-    'd': (0, -1, 0, 0),
-    's': (-1, 0, 0, 0)
+    'i': (1, 0),
+    'o': (1, -1),
+    'j': (0, 1),
+    'l': (0, -1),
+    'u': (1, 1),
+    ',': (-1, 0),
+    '.': (-1, 1),
+    'm': (-1, -1),
 }
 
 speedBindings = {
     'q': (1.1, 1.1),
     'z': (.9, .9),
-    'e': (1.1, 1),
-    'c': (.9, 1),
+    'w': (1.1, 1),
+    'x': (.9, 1),
+    'e': (1, 1.1),
+    'c': (1, .9),
 }
 
 
-class PublishThread(threading.Thread):
-    def __init__(self, rate):
-        super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
-        self.condition = threading.Condition()
-        self.done = False
-
-        # Set timeout to None if rate is 0 (causes new_message to wait forever
-        # for new data to publish)
-        if rate != 0.0:
-            self.timeout = 1.0 / rate
-        else:
-            self.timeout = None
-
-        self.start()
-
-    def wait_for_subscribers(self):
-        i = 0
-        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
-            if i == 4:
-                print("Waiting for subscriber to connect to {}".format(
-                    self.publisher.name))
-            rospy.sleep(0.5)
-            i += 1
-            i = i % 5
-        if rospy.is_shutdown():
-            raise Exception(
-                "Got shutdown request before subscribers connected")
-
-    def update(self, x, y, z, th, speed, turn):
-        self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
-        # Notify publish thread that we have a new message.
-        self.condition.notify()
-        self.condition.release()
-
-    def stop(self):
-        self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
-        self.join()
-
-    def run(self):
-        twist = Twist()
-        while not self.done:
-            self.condition.acquire()
-            # Wait for a new message or timeout.
-            self.condition.wait(self.timeout)
-
-            # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
-
-            self.condition.release()
-
-            # Publish.
-            self.publisher.publish(twist)
-
-        # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist)
-
-
-def getKey(key_timeout):
+def getKey():
     tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
     if rlist:
         key = sys.stdin.read(1)
     else:
         key = ''
+
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
+
+
+speed = .2
+turn = 1
 
 
 def vels(speed, turn):
@@ -141,62 +97,90 @@ def vels(speed, turn):
 if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
 
-    rospy.init_node('teleop_twist_keyboard')
-
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.0)
-    if key_timeout == 0.0:
-        key_timeout = None
-
-    pub_thread = PublishThread(repeat)
+    rospy.init_node('turtlebot_teleop')
+    pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)
 
     x = 0
-    y = 0
-    z = 0
     th = 0
     status = 0
-
+    count = 0
+    acc = 0.1
+    target_speed = 0
+    target_turn = 0
+    control_speed = 0
+    control_turn = 0
     try:
-        pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
-
         print(msg)
         print(vels(speed, turn))
         while(1):
-            key = getKey(key_timeout)
+            key = getKey()
             if key in moveBindings.keys():
                 x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
+                th = moveBindings[key][1]
+                count = 0
             elif key in speedBindings.keys():
                 speed = speed * speedBindings[key][0]
                 turn = turn * speedBindings[key][1]
+                count = 0
 
                 print(vels(speed, turn))
                 if (status == 14):
                     print(msg)
                 status = (status + 1) % 15
-            else:
-                # Skip updating cmd_vel if key timeout and robot already
-                # stopped.
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
-                    continue
+            elif key == ' ' or key == 'k':
                 x = 0
-                y = 0
-                z = 0
                 th = 0
+                control_speed = 0
+                control_turn = 0
+            else:
+                count = count + 1
+                if count > 4:
+                    x = 0
+                    th = 0
                 if (key == '\x03'):
                     break
 
-            pub_thread.update(x, y, z, th, speed, turn)
+            target_speed = speed * x
+            target_turn = turn * th
+
+            if target_speed > control_speed:
+                control_speed = min(target_speed, control_speed + 0.02)
+            elif target_speed < control_speed:
+                control_speed = max(target_speed, control_speed - 0.02)
+            else:
+                control_speed = target_speed
+
+            if target_turn > control_turn:
+                control_turn = min(target_turn, control_turn + 0.1)
+            elif target_turn < control_turn:
+                control_turn = max(target_turn, control_turn - 0.1)
+            else:
+                control_turn = target_turn
+
+            twist = Twist()
+            twist.linear.x = control_speed
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = control_turn
+            pub.publish(twist)
+
+            #print("loop: {0}".format(count))
+            #print("target: vx: {0}, wz: {1}".format(target_speed, target_turn))
+            #print("publihsed: vx: {0}, wz: {1}".format(twist.linear.x, twist.angular.z))
 
     except Exception as e:
         print(e)
 
     finally:
-        pub_thread.stop()
+        twist = Twist()
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+        pub.publish(twist)
 
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
